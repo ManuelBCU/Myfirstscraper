63770a92870d538d2af7e5458589524ce30ebdef
ManuelBCU <43589178+ManuelBCU@users.noreply.github.com>
2019-02-19 11:56:50 +0000

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -12,7 +12,7 @@ print(html)
 #
 # # Find something on the page using css selectors
 root = lxml.html.fromstring(html)
-print(root.cssselect("div[align='left']"))
+print(root.cssselect("a"))
 #
 # # Write out to the sqlite database using scraperwiki library
 # scraperwiki.sqlite.save(unique_keys=['name'], data={"name": "susan", "occupation": "software developer"})
